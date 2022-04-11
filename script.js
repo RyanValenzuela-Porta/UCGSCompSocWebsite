5ccb45fe3f1486360048d2e198001b74d63d2c8b
Ryan Valenzuela-Porta <16valenzuela-portar@uptoncourtgrammar.org.uk>
2022-04-11 16:19:04 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//when logo is clicked, it will redirect to ucgs website
+function redirect(link){
+    window.location.href=link
+  }
+  //when invalid link is clicked it will alert the user
+  function notfound(){
+    alert("Page not Found")
+  }
